17dc2aa0c492785398723cf1e3e7cf6ac7f4de68
Lars-Erik Aabech <lea@markedspartner.no>
2020-10-11 21:25:07 +0200

    Removed spring-boot-devtools. Added parent pom.

---
+++ src/chatroom.web/src/test/java/Persisting_in_web_module.java
@@ -0,0 +1,103 @@
+import org.hiof.chatroom.commands.SendMessageCommand;
+import org.hiof.chatroom.core.ChatMessage;
+import org.hiof.chatroom.database.ChatMessageRepository;
+import org.hiof.chatroom.database.DatabaseManager;
+import org.hiof.chatroom.database.PersistenceFactory;
+import org.hiof.chatroom.database.UnitOfWork;
+import org.hiof.chatroom.notification.NotificationService;
+import org.hiof.chatroom.notification.NotificationServiceFactory;
+import org.hiof.chatroom.web.ChatUIController;
+import org.hiof.chatroom.web.NotificationDispatcher;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.ui.Model;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.Map;
+import java.util.UUID;
+
+public class Persisting_in_web_module {
+
+    @BeforeEach
+    public void reset_database() {
+        java.io.File dbFile = new File("./db/chat.db");
+        dbFile.delete();
+    }
+
+    @Test
+    public void stores_message() throws Exception {
+        PersistenceFactory.Instance = new PersistenceFactory();
+        NotificationServiceFactory.Instance = new NotificationServiceFactory() {
+            @Override
+            public NotificationService getService() {
+                return new NotificationService() {
+                    @Override
+                    public void notifyNewMessage(ChatMessage message) {
+                    }
+                };
+            }
+        };
+
+        Model model = new Model() {
+            @Override
+            public Model addAttribute(String s, Object o) {
+                return null;
+            }
+
+            @Override
+            public Model addAttribute(Object o) {
+                return null;
+            }
+
+            @Override
+            public Model addAllAttributes(Collection<?> collection) {
+                return null;
+            }
+
+            @Override
+            public Model addAllAttributes(Map<String, ?> map) {
+                return null;
+            }
+
+            @Override
+            public Model mergeAttributes(Map<String, ?> map) {
+                return null;
+            }
+
+            @Override
+            public boolean containsAttribute(String s) {
+                return false;
+            }
+
+            @Override
+            public Object getAttribute(String s) {
+                return null;
+            }
+
+            @Override
+            public Map<String, Object> asMap() {
+                return null;
+            }
+        };
+        ChatUIController ctrlr = new ChatUIController(new NotificationDispatcher(new SimpMessagingTemplate(new MessageChannel() {
+            @Override
+            public boolean send(Message<?> message, long l) {
+                return true;
+            }
+        })));
+
+        ctrlr.postMessage("Luke", "Noooo");
+
+        org.hiof.chatroom.persistence.UnitOfWork uow = PersistenceFactory.Instance.createUnitOfWork();
+        org.hiof.chatroom.persistence.ChatMessageRepository repo = PersistenceFactory.Instance.createChatMessageRepository(uow);
+
+        ChatMessage msg = repo.query().findFirst().get();
+        Assertions.assertEquals("Luke", msg.getUser());
+        Assertions.assertEquals("Noooo", msg.getMessage());
+    }
+}
