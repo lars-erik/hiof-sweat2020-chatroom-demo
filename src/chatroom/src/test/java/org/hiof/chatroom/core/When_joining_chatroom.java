49c9bcfc82aff028eda39cf7602724026b4239c5
Lars-Erik <lea@markedspartner.no>
2022-10-23 13:11:16 +0200

    Failing last 20 test - we don't have time

---
+++ src/chatroom/src/test/java/org/hiof/chatroom/core/When_joining_chatroom.java
@@ -0,0 +1,11 @@
+package org.hiof.chatroom.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class When_joining_chatroom {
+    @Test
+    public void last_20_messages_are_shown() {
+        Assertions.fail("Not done");
+    }
+}
